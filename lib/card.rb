5d327362c0087f10ad68692caf4d59a4d5abb6a5
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-05 18:40:24 +0100

    Added two test examples for Card

--- lib/card.rb
+++ lib/card.rb
@@ -2,9 +2,10 @@
 
 # Class card
 class Card
-  attr_reader :type
+  attr_reader :rank, :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
