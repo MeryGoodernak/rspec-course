5d248f965b423e904462bc4ca8b2eedc9efbd898
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-11 17:39:02 +0100

    Amend code using single qoute for strings

---
+++ spec/before_after_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe 'before and after hooks' do
+  it 'is just a random example' do
+    expect(5 * 4).to eq(20)
+  end
+
+  it 'is just another random example' do
+    expect(15 / 3).to eq(5)
+  end
+end
