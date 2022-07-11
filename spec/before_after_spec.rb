a28df2d518a65cd533d5c9f7f361ebdbbd35f8be
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-11 17:49:41 +0100

    Wrote some tests using before and after hooks

--- spec/before_after_spec.rb
+++ spec/before_after_spec.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe 'before and after hooks' do
+  before(:context) do
+    puts 'Start running the tests!'
+  end
+
+  after(:context) do
+    puts 'End of the all examples!'
+  end
+
+  before(:example) do
+    puts 'Hello before each example!'
+  end
+
+  after(:example) do
+    puts 'Buy after each example!'
+  end
+
   it 'is just a random example' do
     expect(5 * 4).to eq(20)
   end
