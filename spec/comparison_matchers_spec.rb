7efcea7d94a7da82463ef198e0014c9673f1fd66
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-14 17:29:04 +0100

    Practiced comparison matcher

--- spec/comparison_matchers_spec.rb
+++ spec/comparison_matchers_spec.rb
@@ -1,3 +1,14 @@
 RSpec.describe 'comprison matchers' do
+  it 'allows for comparison with built-in Ruby operators' do
+    expect(10).to be > 9
+    expect(11).to be < 30
 
+    expect(22).to be >= 22
+    expect(22).to be >= 21
+
+    expect(2).to be <= 2
+    expect(22).to be <= 23
+
+    expect(22).not_to be > 23
+  end
 end
