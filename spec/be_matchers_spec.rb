6f51edd6a52015a698fd837e4f9e525142ccf0a8
Maryam Fazlali <fazlali1202@gmail.com>
2022-07-18 15:40:20 +0100

    Adding some truthiness assertions

--- spec/be_matchers_spec.rb
+++ spec/be_matchers_spec.rb
@@ -3,5 +3,14 @@
 RSpec.describe 'be truthy or falsy matchers' do
   it 'test the truthiness' do
     expect(5).to be_truthy
+    expect(0).to be_truthy
+    expect('').to be_truthy
+    expect([]).to be_truthy
+    expect({}).to be_truthy
+    expect(:symbol).to be_truthy
+  end
+
+  describe ['e'] do
+    it { is_expected.to be_truthy }
   end
 end
