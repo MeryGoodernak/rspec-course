a68cdc3132e1708af760a6f4c4eae52e4a664df0
Maryam Fazlali <fazlali1202@gmail.com>
2022-08-01 17:13:57 +0100

    double assignment

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -24,3 +24,17 @@ RSpec.describe 'double' do
     end
   end
 end
+
+
+RSpec.describe 'doubles-assignment' do
+  it "tests the student's knowledge of the course's content" do
+    db = double('Database Connection', connect: true, disconnect: 'Goodbye')
+    expect(db.connect).to be true
+    expect(db.disconnect).to eq('Goodbye')
+
+    fs = double('File System')
+    allow(fs).to receive_messages(read: 'Romeo and Juliet', write: false)
+    expect(fs.read).to eq('Romeo and Juliet')
+    expect(fs.write).to be false
+  end
+end
