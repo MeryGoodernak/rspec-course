e1115420a3d4cc199fcfe8cbb64e95b10e27e9b9
Maryam Fazlali <fazlali1202@gmail.com>
2022-08-01 16:40:43 +0100

    Double second edition

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'double' do
   context 'first edition' do
-    it 'allows defined method to be invoked' do
+    it 'allows defined methods to be invoked' do
       stuntman = double('Mr. Danger', fall_in_fire: 'Ouch', jump_on_fire: true)
       expect(stuntman.fall_in_fire).to eq('Ouch')
       expect(stuntman.jump_on_fire).to be true
@@ -8,10 +8,19 @@ RSpec.describe 'double' do
   end
 
   context 'second edition' do
-    it 'allows defined method to be invoked-second edition' do
+    it 'allows defined methods to be invoked-second edition' do
       stuntman = double('Mr. Danger')
       allow(stuntman).to receive(:fall_in_fire).and_return('Ouch')
       expect(stuntman.fall_in_fire).to eq('Ouch')
+    end
+  end
+
+  context 'third edition' do
+    it 'allows defined methods to be invoked-third edition' do
+      stuntman = double('Mr. Danger')
+      allow(stuntman).to receive_messages(fall_in_fire: 'Ouch', jump_on_fire: true)
+      expect(stuntman.fall_in_fire).to eq('Ouch')
+      expect(stuntman.jump_on_fire).to be true
     end
   end
 end
