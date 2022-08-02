3af869bf5ad2a96134edc754b246de3ab8ebfaf1
Maryam Fazlali <fazlali1202@gmail.com>
2022-08-02 12:10:47 +0100

    Practicing allow method with pairing return value

--- spec/allow_method_spec.rb
+++ spec/allow_method_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe 'allow method' do
     allow(calculator).to receive(:add)
     expect(calculator.add).to be_nil
   end
+
+  it 'can customize return value for methods on double no matter how many argument the method takes' do
+    calculator = double
+    allow(calculator).to receive(:add).and_return(15)
+    expect(calculator.add).to eq(15)
+    expect(calculator.add(2)).to eq(15)
+    expect(calculator.add(3, 6)).to eq(15)
+    expect(calculator.add('hello')).to eq(15)
+  end
 end
