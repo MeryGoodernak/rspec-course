e46f3ec5f09a4b2cc7e7f6d0b7030ef6fe74b849
Maryam Fazlali <fazlali1202@gmail.com>
2022-08-02 17:41:01 +0100

    Practicing spy method with dependency injection

--- spec/spies_spec.rb
+++ spec/spies_spec.rb
@@ -1,22 +1,22 @@
-# class Food
-#   def eat
-#     puts 'ate'
-#   end
+class Food
+  def eat
+    puts 'ate'
+  end
 
-#   def drink
-#     'drunk'
-#   end
-# end
+  def drink
+    'drunk'
+  end
+end
 
-# class Animal
-#   def initialize(food)
-#     @food = food
-#   end
+class Animal
+  def initialize(food)
+    @food = food
+  end
 
-#   def eat_food
-#     @food.eat
-#   end
-# end
+  def eat_food
+    @food.eat
+  end
+end
 
 RSpec.describe 'spies' do
   let(:animal) { Animal.new(food) }
@@ -25,5 +25,16 @@ RSpec.describe 'spies' do
   it 'confirms that a message has been recieved' do
     animal.eat_food
     expect(food).to have_received(:eat)
+  end
+
+  it 'resets between examples' do
+    expect(food).not_to have_received(:eat)
+  end
+
+  it 'retains the same functionality like double' do
+    animal.eat_food
+    animal.eat_food
+    food.eat('Water')
+    expect(food).to have_received(:eat).exactly(3).times
   end
 end
